291444a3c709f1fef25f8bffc47795b67783811f
Christine <cylee4@gmail.com>
2015-01-16 16:12:38 -0800

    wip

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,14 @@
-angular.module('shortly.links', [])
+
+ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = [
+    {'url': 'test url',
+     'title': 'test title',
+     'shortcode': 'test shortcode'
+    }
+  ];
+  $scope.getLinks = function(){};
+
 });
